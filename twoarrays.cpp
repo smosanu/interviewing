06c6d0aaf0f3fbaeb4e97240da73422916588d13
Sergiu Mosanu <sm7ed@virginia.edu>
2023-08-10 16:40:18 -0400

    beautify

--- twoarrays.cpp
+++ twoarrays.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-int count_matching(int *a1, int lena1, int *a2, int lena2){
-  int count = 0;
+int count_matching(int *a1, int lena1, int *a2, int lena2)
+{
+    int count = 0;
 
-    cout << "a1.len: " << lena1 << endl;
-    cout << "a2.len: " << lena2 << endl;
+    // cout << "a1.len = " << lena1 << endl;
+    // cout << "a2.len = " << lena2 << endl;
 
     int i1 = 0;
     int i2 = 0;
     while ((i1 < lena1) && (i2 < lena2))
     {
-        cout << i1 << i2 << endl;
+        // cout << "idx1=" << i1 << " idx2=" << i2 << endl;
         if (a1[i1] == a2[i2])
         {
             count++;
@@ -20,28 +21,41 @@ int count_matching(int *a1, int lena1, int *a2, int lena2){
             i1++;
             i2++;
         }
-        else if (a1[i1] < a2[i2]){
+        else if (a1[i1] < a2[i2])
+        {
             i1++;
         }
-        else {
+        else
+        {
             i2++;
         }
     }
-    cout << "Count = " << count << endl;
-  return count;
+    return count;
 }
 
 int main()
 {
     // two arrays, sorted, distinct
-    int a1[] = {0, 1, 5, 8, 23, 67, 89, 456, 890, 9004, 9999, };
+    int a1[] = {
+        0,
+        1,
+        5,
+        8,
+        23,
+        67,
+        89,
+        456,
+        890,
+        9004,
+        9999,
+    };
     int a2[] = {1, 4, 8, 78, 89, 234, 459, 679, 2469, 9999, 101010};
-    // int count = 0;
 
     int lena1 = sizeof(a1) / sizeof(int);
     int lena2 = sizeof(a2) / sizeof(int);
 
-    cout << "Count = " << count_matching(a1, lena1, a2, lena2) << endl;
-    
+    int matching = count_matching(a1, lena1, a2, lena2);
+    cout << "Number of matching values = " << matching << endl;
+
     return (0);
 }
