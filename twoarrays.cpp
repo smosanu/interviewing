bb252e23e58d5a4b63023681f23be3e36235e55c
Sergiu Mosanu <sm7ed@virginia.edu>
2023-08-10 16:34:08 -0400

    do some verification and implement function call

--- twoarrays.cpp
+++ twoarrays.cpp
@@ -2,20 +2,15 @@
 
 using namespace std;
 
-int main()
-{
-    // two arrays, sorted, distinct
-    int a1[] = {0, 1, 5, 8, 23, 67, 89, 456, 890, 9004, 9999, };
-    int a2[] = {1, 4, 8, 78, 89, 234, 459, 679, 2469, 9999, 101010};
-    int count = 0;
+int count_matching(int *a1, int lena1, int *a2, int lena2){
+  int count = 0;
 
-    cout << "a1.size: " << sizeof(a1) / sizeof(int) << endl;
-    cout << "a2.size: " << sizeof(a2) / sizeof(int) << endl;
+    cout << "a1.len: " << lena1 << endl;
+    cout << "a2.len: " << lena2 << endl;
 
-    // for (int i1 = 0; i1 < sizeof(a1) / sizeof(int); i1++)
     int i1 = 0;
     int i2 = 0;
-    while ((i1 < sizeof(a1) / sizeof(int)) & (i2 < sizeof(a2) / sizeof(int)))
+    while ((i1 < lena1) && (i2 < lena2))
     {
         cout << i1 << i2 << endl;
         if (a1[i1] == a2[i2])
@@ -33,5 +28,20 @@ int main()
         }
     }
     cout << "Count = " << count << endl;
+  return count;
+}
+
+int main()
+{
+    // two arrays, sorted, distinct
+    int a1[] = {0, 1, 5, 8, 23, 67, 89, 456, 890, 9004, 9999, };
+    int a2[] = {1, 4, 8, 78, 89, 234, 459, 679, 2469, 9999, 101010};
+    // int count = 0;
+
+    int lena1 = sizeof(a1) / sizeof(int);
+    int lena2 = sizeof(a2) / sizeof(int);
+
+    cout << "Count = " << count_matching(a1, lena1, a2, lena2) << endl;
+    
     return (0);
 }
